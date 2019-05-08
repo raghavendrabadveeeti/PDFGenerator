a2d54f188a876ecb9c8b5200cb01a3b3ed320245
Raghavendra Badveeti <raghavendra.badveeti@jda.com>
2019-05-08 17:34:32 +0530

    Code Refactoring

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var path = require('path');
 //var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var fs = require('fs');
-var rfs = require('rotating-file-stream')
+var rfs = require('rotating-file-stream');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var pagePreview = require('./routes/pagePreview');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,8 +33,6 @@ var accessLogStream = rfs('pdfgenerator.log', {
 
 app.use('/', indexRouter);
 app.use('/pdf', pagePreview);
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/pagePreview.js
+++ routes/pagePreview.js
@@ -7,112 +7,77 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
       (async () => {
-        //await page.goto('https://flyers.smartcanucks.ca/canada/loblaws-on-flyer-april-4-to-102/book/2',  {
-        //await page.goto('https://twitter.com/narendramodi?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',  {
 
-        let random = Math.floor(Math.random() * 1000000000000);
-
+        let pdf = printPDF(req, res);
+        let random = getRandom();
+        let fileName = 'page' + random + '.pdf';
+        const browser = await puppeteer.launch({headless: true, pipe: true, ignoreHTTPSErrors: true})
         try {
-          console.log(random, "Total Process Start", new Date())
-          const browser = await puppeteer.launch({headless: true, pipe: true, ignoreHTTPSErrors: true})
-          const page = await browser.newPage()
+          console.log(random, "Total Process Start", new Date());
 
-/*          const path = require('path');
-          console.log(path.dirname(require.main));
-          console.log(path.dirname(require.main));
-          console.log(path.dirname(require.main));
-          console.log(path.dirname(require.main));*/
-          //throw Error("Failed .. ");
-          /*          let queryString = req._parsedUrl.search;
-                    pageIds = req.query.pageIDs;
-                    storeGroupIDs = req.query.storeGroupIDs;
-                    console.log(pageIds);
-                    console.log(storeGroupIDs);
-                    pages = pageIds.split(",");
-                    storeGroups = storeGroupIDs.split(",");
+          const page = await browser.newPage();
+          //await page.setViewport({width: 1920, height: 1080});
+          let queryString = req._parsedUrl.search;
 
-                    let count = pages.length * storeGroups.length;
-                    console.log(count);*/
+          // console.log(queryString);
 
-          /*     await page.goto('http://localhost:1841/', {
-                  waitUntil: "networkidle0"
-               }) */
-          //await page.goto('file:///C:\\ADV\\admanpdf\\source.htm', {
-
-          var dir = './tmp';
-
-          /*          console.log(__dirname);
-                    console.log(__dirname);
-                    console.log(__dirname);
-                    console.log(__dirname);*/
-
-          if (!fileSystem.existsSync(dir)) {
-            fileSystem.mkdirSync(dir);
-          }
-
-          let pdfFiles = [];
-          for (var i = 0; i < 2; i++) {
-            console.log("Start child", i, new Date())
-            await page.goto('http://localhost:4200/home', {waitUntil: 'networkidle0'});
-            var pdfFileName = 'ad_' + random + '_' + (i + 1) + '.pdf';
-            pdfFiles.push(pdfFileName);
-            await page.pdf({path: pdfFileName, format: 'A4'});
-            console.log("completed child", i, new Date())
-          }
-
-          /* await page.goto('http://localhost:4200/home', {
-             waitUntil: "networkidle0",
-             timeout  : 3000000
-           });
-           const html = await page.content();
-
-           console.log(html);*/
-          await browser.close();
-
-          await mergeMultiplePDF(pdfFiles, random);
-
-          /*const buffer = await page.pdf(
+          await page.goto('http://localhost:8885/' + queryString, {
+            waitUntil: "networkidle0",
+            waitLoad : true,
+            timeout  : 0
+          });
+          const pdf = await page.pdf(
               {
-                path           : 'page.pdf',
-                format         : 'A4',
+                //margin         : {left: '2cm', top: '4cm', right: '1cm', bottom: '2.5cm'},
+                format         : 'A2',
                 printBackground: true
-              })
-              */
-          res.type('application/pdf')
-          var readStream = fileSystem.createReadStream('adFinal_' + random + '.pdf');
-          // We replaced all the event handlers with a simple call to readStream.pipe()
-          readStream.pipe(res);
-          //res.send('success')
-          //browser.close()
+              });
+
+          res.type('application/pdf');
+          res.send(pdf);
           console.log(random, "Total Process  End", new Date())
         } catch (e) {
           console.log(e, 'here I am');
-          browser.close()
           res.statusCode = 500;
           res.send('Error while processing PDF');
-          //next(e);
+        } finally {
+          if (browser) {
+            browser.close();
+          }
         }
 
       })()
     }
 );
 
+async function printPDF(req, res) {
+  const browser = await puppeteer.launch({headless: true, ignoreHTTPSErrors: true});
+  const page = await browser.newPage();
+  await page.goto('https://blog.risingstack.com', {waitUntil: 'networkidle0'});
+  const pdf = await page.pdf({format: 'A4'});
+
+  await browser.close();
+  return pdf
+}
+
 const mergeMultiplePDF = (pdfFiles, random) => {
-      return new Promise((resolve, reject) => {
-            merge(pdfFiles, 'adFinal_' + random + '.pdf', function (err) {
+  return new Promise((resolve, reject) => {
+        merge(pdfFiles, 'adFinal_' + random + '.pdf', function (err) {
 
-              if (err) {
-                console.log(err);
-                reject(err)
-              }
-
-              console.log('Success');
-              resolve()
-            });
+          if (err) {
+            console.log(err);
+            reject(err)
           }
-      )
-          ;
-    }
-;
+
+          console.log('Success');
+          resolve()
+        });
+      }
+  );
+};
+
+function getRandom() {
+  return Math.floor(Math.random() * 1000000000000);
+}
 
 module.exports = router;
